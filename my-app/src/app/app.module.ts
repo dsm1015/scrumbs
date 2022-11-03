185c7853d1585ebe94492cc8a94e55d5a6aa94aa
lukemcelligott <lpm1006@gmail.com>
2022-11-02 20:04:17 -0400

    updated login page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './public/login/login.component';
+import { DashboardComponent } from './protected/dashboard/dashboard.component';
 
 //Angular Material Imports
 import { FlexLayoutModule } from '@angular/flex-layout'; //flex layout
@@ -13,12 +14,13 @@ import { MatFormFieldModule } from '@angular/material/form-field'; //forms
 import { MatInputModule } from '@angular/material/input'; //inputs
 import { MatButtonModule } from '@angular/material/button'; //buttons
 import { MatCardModule } from '@angular/material/card'; //cards
-
+import { MatToolbarModule } from '@angular/material/toolbar'; //toolbar
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +30,8 @@ import { MatCardModule } from '@angular/material/card'; //cards
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
